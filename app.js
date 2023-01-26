0f48d5c1e36b772cd46e19f1e19b25b2d017e62b
Connor Johnson <connorjohnson211@gmail.com>
2023-01-26 18:49:14 -0500

    image customization

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const app = express();
 
 const port = 3000;
 
-// Statc files
+// Static files
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 
 // Set Templating Engine
-app.set('view engine', 'ejs')
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 //Navigation
-
 app.get('', (req, res) =>{
-    res.sendFile('/views/index.html', {root: __dirname})
+    res.render('index')
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about',{text: "About PGE"})
 })
 
 //Listen on Port 3000
